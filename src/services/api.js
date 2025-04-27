db10371f6167b9a14d31fc6f20f648b845ac1492
Conner Krenzer <krenzer84@gmail.com>
2025-04-27 13:32:08 -0400

    first commit

---
+++ src/services/api.js
@@ -0,0 +1,2 @@
+// # API calls and backend integration
+// # Axios instance and API methods
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// filepath: c:\Users\sk8te\Desktop\book-recommender-frontend\tailwind.config.js
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class", // Enable dark mode
+  plugins: [],
+};
